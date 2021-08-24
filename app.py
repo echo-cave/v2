16f6d843a145d554e5fcd358ccb35834e9df981c
RDPStudio <67402215+rdp-studio@users.noreply.github.com>
2021-08-24 15:17:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ print("Updateing Database")
 url = 'https://cdn.jsdelivr.net/gh/echo-cave/cave@latest/cave.txt' 
 r = requests.get(url)
 with open("cave.txt", "wb") as f:
-  f.write(r.content)
+  f.write(str(r.content).strip())
 
 def get_cave():
     txt = open(os.getcwd()+'/cave.txt','rb')
